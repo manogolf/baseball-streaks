9879381c52d7d0d4eb6039b89cb5010813152c49
manogolf <manogolf@gmail.com>
2025-05-15 08:47:32 -0700

    path fix

--- scripts/fetchHotStreaks.js
+++ scripts/fetchHotStreaks.js
@@ -1,6 +1,6 @@
 // src/scripts/fetchHotStreaks.js
 
-import { supabase } from "../utils/supabaseUtils.js";
+import { supabase } from "../src/utils/supabaseUtils.js";
 import fetch from "node-fetch";
 
 // Settings
