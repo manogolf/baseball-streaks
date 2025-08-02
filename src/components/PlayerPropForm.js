001638fff6cf924391362d215b188e7492c59fa4
manogolf <manogolf@gmail.com>
2025-08-01 17:28:12 -0700

    testing localhost behavior

--- src/components/PlayerPropForm.js
+++ src/components/PlayerPropForm.js
@@ -7,10 +7,12 @@ import { useAuth } from "../context/AuthContext.js";
 import { getPropTypeOptions } from "../../shared/propUtils.js";
 import { enrichGameContext } from "../../shared/enrichGameContext.js";
 
-const isLocal = window.location.hostname === "localhost";
-const apiUrl = isLocal
-  ? "http://localhost:3001"
-  : "https://express-api-vm4c.onrender.com";
+// const isLocal = window.location.hostname === "localhost";
+// const apiUrl = isLocal
+//  ? "http://localhost:3001"
+//  : "https://express-api-vm4c.onrender.com";
+
+const apiUrl = "https://express-api-vm4c.onrender.com";
 
 const PlayerPropForm = ({ onPropAdded }) => {
   const today = todayET();
